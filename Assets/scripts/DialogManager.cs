a5bb445754cf7efb79ccc506ebee8b5d55165d8d
comunicacao <comunicacao@animaedu.intranet>
2019-10-19 21:06:52 -0300

    iniciacao dos projetos

---
+++ Assets/scripts/DialogManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DialogManager : MonoBehaviour
+{
+    int State;
+    GameObject sidePanel;
+    Animator anim;
+
+    public GameObject dialogObj;
+    public GameObject nameText;
+    public GameObject MessageText;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public void OpenSidePanel()
+    {
+
+        if(State < 1)
+        {
+            State++;
+        }
+        else
+        {
+            State = 0;
+            dialogObj.SetActive(true);
+        }
+        anim.SetInteger("State", State);
+    }
+
+    public void hideDialog()
+    {
+        dialogObj.SetActive(false);
+    }
+
+    public void showDialog()
+    {
+        dialogObj.SetActive(true);
+    }
+
+    public void hideSpecs()
+    {
+        nameText.SetActive(false);
+        MessageText.SetActive(false);
+    }
+
+    public void showSpecs()
+    {
+        nameText.SetActive(true);
+        MessageText.SetActive(true);
+    }
+
+
+}
---
+++ Assets/scripts/TitleManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleManager : MonoBehaviour
+{
+    int State;
+    GameObject sidePanel;
+    Animator anim;
+
+    public GameObject sobre;
+    public GameObject comoJogar;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public void OpenSidePanel()
+    {
+
+        if(State < 1)
+        {
+            State++;
+            sobre.SetActive(true);
+            comoJogar.SetActive(false);
+        }
+        else
+        {
+            State = 0;
+            sobre.SetActive(false);
+
+        }
+        anim.SetInteger("State", State);
+    }
+
+    public void OpenSidePanel2()
+    {
+        if (State < 1)
+        {
+            State++;
+            comoJogar.SetActive(true);
+            sobre.SetActive(false);
+        }
+        else
+        {
+            State = 0;
+            
+            comoJogar.SetActive(false);
+        }
+        anim.SetInteger("State", State);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("fase 1");
+    }
+
+
+}
